f9a5e1995721ee7a22111f5ff1f200c7d3e5e54f
mgreen1092 <morgan.green0209@gmail.com>
2023-01-19 15:46:50 -0500

    input and hero added to hero-detail.component.spec.ts

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,6 +1,8 @@
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroDetailComponent } from './hero-detail.component';
+
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
