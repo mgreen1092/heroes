69cf5403ea1cf0711fc6defc7b2ed51ca9a6929f
mgreen1092 <morgan.green0209@gmail.com>
2023-01-19 16:11:10 -0500

    getHeroes method added to hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+// ng generate service hero geenrates a skeleton HeroService class
+import {Hero} from './hero'
+import { HEROES } from './mock-heroes';
 @Injectable({
   providedIn: 'root'
 })
+// HeroService class is going to provide an injectable service and it can also have its own
+// injected dependencies 
+// @Injectable accepts metadata object for the service, the same way the @Component decorator did
 export class HeroService {
-
-  constructor() { }
+  getHeroes(): Hero[] {
+    return HEROES
+  }
+  constructor() { 
+  }
 }
