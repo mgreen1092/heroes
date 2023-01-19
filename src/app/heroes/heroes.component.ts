07e69cbe0e9a63832bacd0f649d6db6c131e7e28
mgreen1092 <morgan.green0209@gmail.com>
2023-01-19 11:00:27 -0500

    hero name appears

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+//always import component from @angular/core, gives the metedata properties:
+//selector, templateUrl, and styleUrls
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-heroes', //matches the name of the HTML element that identifies the componenet
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-
+//always export the component class, like in React, so you can import it elsewhere (AppModule)
+ hero = 'Winstrom'
 }
+
