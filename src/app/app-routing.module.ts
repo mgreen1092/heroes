071753287e9d72112620e11e4881f4c8eb9a5f21
mgreen1092 <morgan.green0209@gmail.com>
2023-01-20 09:55:15 -0500

    removed hero details component from hero component and added herodetail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 // gives the router somewhere to go once you configure the routes
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent},
@@ -11,8 +12,9 @@ const routes: Routes = [
   // address) and a component (what the router should create when navigating to this route)
   {path: 'dashboard', component: DashboardComponent},
   // sets up the dashbaord route
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   // sets up a redirect to the dashboard component
+  {path: 'detail/:id', component: HeroDetailComponent}
 ];
 
 @NgModule({
