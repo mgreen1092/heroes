a94d65bbe8e93aef62150637dfe2a673e39c77ff
mgreen1092 <morgan.green0209@gmail.com>
2023-01-20 10:05:46 -0500

    removed unused code in HeroComponent class

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,8 +21,9 @@ export class HeroesComponent implements OnInit{
 //  }
   selectedHero?: Hero;
   heroes: Hero [] = []
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
   // defines a private heroService property and identifies it as a HeroService injection site
+  // removed private messageService: MessageService after adjusting for routes
   getHeroes(): void {
     // this.heroes= this.heroService.getHeroes() --> removed after adding observables
     // retrieved heroes from the service 
@@ -36,10 +37,11 @@ export class HeroesComponent implements OnInit{
     this.getHeroes()
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
     // assigns the clicked hero from the template to the component's selectedHero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
+    // this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+    // removed after adding routes
+  // }
 }
 
