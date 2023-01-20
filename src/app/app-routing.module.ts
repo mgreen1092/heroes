2540a31906bc3d4efa7677a4be0c029ef4c3190c
mgreen1092 <morgan.green0209@gmail.com>
2023-01-20 09:50:27 -0500

    dashboard route set up and redirect to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 //enables routing capacity
 import { HeroesComponent } from './heroes/heroes.component';
 // gives the router somewhere to go once you configure the routes
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path: 'heroes', component: HeroesComponent},
   // an angular route has two properties, a path (string that matches the URL in the browser 
   // address) and a component (what the router should create when navigating to this route)
+  {path: 'dashboard', component: DashboardComponent},
+  // sets up the dashbaord route
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  // sets up a redirect to the dashboard component
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,12 +11,16 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit{
   heroes: Hero [] = []
+  // defines a heroes array property 
   constructor (private heroService: HeroService) {}
+  // constructor expects Angular to inject the HeroService into a private heroService property
   ngOnInit(): void {
     this.getHeroes()
   }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5))
+      // returns the sliced list of heroes at positions 1 and 5, returning only Heroes two, 
+      // three, four and five
   }
 }
